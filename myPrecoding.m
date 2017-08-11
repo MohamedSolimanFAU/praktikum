b58dabcb39830e4d93ad7d46d5f91bd74b4bc3c6
Mohamed Soliman <mohamed.soliman@fau.de>
2017-08-11 13:33:35 +0200

    strict to k & j user index

--- myPrecoding.m
+++ myPrecoding.m
@@ -22,64 +22,64 @@ count = 10;
 
 %% Initializing v_MMSE_k & g_MMSE_k
 
-for i_user = 1:N_user
-    v_init{i_user} = randn(Nt, 1)+ 1i* randn(Nt, 1);
-    V_init{i_user} = fft(v_init{i_user}, N, 3);
-    V{i_user}      = zeros(Nt, 1, N);
-    G{i_user}      = zeros(Nt, 1, N);
-    G_init{i_user} = zeros(Nt, 1, N);
-    sum_G{i_user}  = zeros(Nr, Nt, N);
-    sum_V{i_user}  = zeros(Nr, Nt, N);
+for k_user = 1:N_user
+    v_init{k_user} = randn(Nt, 1)+ 1i* randn(Nt, 1);
+    V_init{k_user} = fft(v_init{k_user}, N, 3);
+    V{k_user}      = zeros(Nt, 1, N);
+    G{k_user}      = zeros(Nt, 1, N);
+    G_init{k_user} = zeros(Nt, 1, N);
+    sum_G{k_user}  = zeros(Nr, Nt, N);
+    sum_V{k_user}  = zeros(Nr, Nt, N);
 end
 
 for idx = 1:N
-    for i_user = 1:N_user
+    for k_user = 1:N_user
         for j_user = 1:N_user
-            sum_G{i_user}(:,:,idx)  = sum_G{i_user}(:,:,idx) + H_ch{i_user, j_user}(:,:,idx)*V_init{j_user}(:,:,idx)*V_init{j_user}(:,:,idx)'*H_ch{i_user, j_user}(:,:,idx)';
+            sum_G{k_user}(:,:,idx)  = sum_G{k_user}(:,:,idx) + H_ch{k_user, j_user}(:,:,idx)*V_init{j_user}(:,:,idx)*V_init{j_user}(:,:,idx)'*H_ch{k_user, j_user}(:,:,idx)';
         end
-        G_init{i_user}(:,:,idx) = pinv(sum_G{i_user}(:,:,idx) + VarN(i_user)*eye(Nr)) * H_ch{i_user, i_user}(:,:,idx) * V_init{i_user}(:,:,idx);
+        G_init{k_user}(:,:,idx) = pinv(sum_G{k_user}(:,:,idx) + VarN(k_user)*eye(Nr)) * H_ch{k_user, k_user}(:,:,idx) * V_init{k_user}(:,:,idx);
     end
     
-    for i_user = 1:N_user
+    for k_user = 1:N_user
         for j_user = 1:N_user
-            sum_V{i_user}(:,:,idx)  = sum_V{i_user}(:,:,idx) + H_ch{j_user, i_user}(:,:,idx)'*G_init{j_user}(:,:,idx)*G_init{j_user}(:,:,idx)'*H_ch{j_user, i_user}(:,:,idx);
+            sum_V{k_user}(:,:,idx)  = sum_V{k_user}(:,:,idx) + H_ch{j_user, k_user}(:,:,idx)'*G_init{j_user}(:,:,idx)*G_init{j_user}(:,:,idx)'*H_ch{j_user, k_user}(:,:,idx);
         end
-        V_init{i_user}(:,:,idx) = pinv(sum_V{i_user}(:,:,idx)) * H_ch{i_user, i_user}(:,:,idx)' * G_init{i_user}(:,:,idx);
-        V_norm{i_user}(:,:,idx) = norm(V_init{i_user}(:,:,idx), 2)^2;
+        V_init{k_user}(:,:,idx) = pinv(sum_V{k_user}(:,:,idx)) * H_ch{k_user, k_user}(:,:,idx)' * G_init{k_user}(:,:,idx);
+        V_norm{k_user}(:,:,idx) = norm(V_init{k_user}(:,:,idx), 2)^2;
     end
     
-    for i_user = 1:N_user
-        if V_norm{i_user}(:,:,idx) <= 1
-            V{i_user}(:,:,idx) = V_init{i_user}(:,:,idx);
-        elseif V_norm{i_user}(:,:,idx) > 1
-            lambda{i_user}(1,idx) =  0;
+    for k_user = 1:N_user
+        if V_norm{k_user}(:,:,idx) <= 1
+            V{k_user}(:,:,idx) = V_init{k_user}(:,:,idx);
+        elseif V_norm{k_user}(:,:,idx) > 1
+            lambda{k_user}(1,idx) =  0;
             
             for i = 1:count
-                inverse_mat   = pinv(sum_V{i_user}(:,:,idx) + lambda{i_user}(1,idx)*eye(Nt));
+                inverse_mat   = pinv(sum_V{k_user}(:,:,idx) + lambda{k_user}(1,idx)*eye(Nt));
                 inner_sum_num = inverse_mat * inverse_mat;
                 inner_sum_den = inverse_mat * inverse_mat * inverse_mat;
                 
-                num =     G_init{i_user}(:,:,idx)' * H_ch{i_user, i_user}(:,:,idx)* inner_sum_num * H_ch{i_user, i_user}(:,:,idx)' * G_init{i_user}(:,:,idx) - 1;
-                den = 2 * G_init{i_user}(:,:,idx)' * H_ch{i_user, i_user}(:,:,idx)* inner_sum_den * H_ch{i_user, i_user}(:,:,idx)' * G_init{i_user}(:,:,idx);
+                num =     G_init{k_user}(:,:,idx)' * H_ch{k_user, k_user}(:,:,idx)* inner_sum_num * H_ch{k_user, k_user}(:,:,idx)' * G_init{k_user}(:,:,idx) - 1;
+                den = 2 * G_init{k_user}(:,:,idx)' * H_ch{k_user, k_user}(:,:,idx)* inner_sum_den * H_ch{k_user, k_user}(:,:,idx)' * G_init{k_user}(:,:,idx);
                 
-                lambda{i_user}(1,idx)   = lambda{i_user}(1,idx) + (num/den);
+                lambda{k_user}(1,idx)   = lambda{k_user}(1,idx) + (num/den);
                 
-                V{i_user}(:,:,idx)      = pinv(sum_V{i_user}(:,:,idx) + lambda{i_user}(1,idx)*eye(Nt)) * H_ch{i_user, i_user}(:,:,idx)' * G_init{i_user}(:,:,idx);
-                V_norm{i_user}(:,:,idx) = norm(V{i_user}(:,:,idx), 2)^2;
+                V{k_user}(:,:,idx)      = pinv(sum_V{k_user}(:,:,idx) + lambda{k_user}(1,idx)*eye(Nt)) * H_ch{k_user, k_user}(:,:,idx)' * G_init{k_user}(:,:,idx);
+                V_norm{k_user}(:,:,idx) = norm(V{k_user}(:,:,idx), 2)^2;
                 
-                if round(V_norm{i_user}(:,:,idx), 4) == 1
+                if round(V_norm{k_user}(:,:,idx), 4) == 1
                     break;
                 end
             end
         end
     end
     
-    for i_user = 1:N_user
-        sum_G{i_user}(:,:,idx)  = zeros(Nr, Nt);
+    for k_user = 1:N_user
+        sum_G{k_user}(:,:,idx)  = zeros(Nr, Nt);
         for j_user = 1:N_user
-            sum_G{i_user}(:,:,idx)  = sum_G{i_user}(:,:,idx) + H_ch{i_user, j_user}(:,:,idx)*V{j_user}(:,:,idx)*V{j_user}(:,:,idx)'*H_ch{i_user, j_user}(:,:,idx)';
+            sum_G{k_user}(:,:,idx)  = sum_G{k_user}(:,:,idx) + H_ch{k_user, j_user}(:,:,idx)*V{j_user}(:,:,idx)*V{j_user}(:,:,idx)'*H_ch{k_user, j_user}(:,:,idx)';
         end
-        G{i_user}(:,:,idx) = pinv(sum_G{i_user}(:,:,idx) + VarN(i_user)*eye(Nr)) * H_ch{i_user, i_user}(:,:,idx) * V{i_user}(:,:,idx);
+        G{k_user}(:,:,idx) = pinv(sum_G{k_user}(:,:,idx) + VarN(k_user)*eye(Nr)) * H_ch{k_user, k_user}(:,:,idx) * V{k_user}(:,:,idx);
     end
 end
 
